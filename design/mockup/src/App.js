5c6cfec8b47591fd34001e6f870316921c24096f
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-10-08 14:29:44 +0400

    Good stab at adding preference blocks

--- design/mockup/src/App.js
+++ design/mockup/src/App.js
@@ -3,13 +3,26 @@ import styles from "./App.module.scss";
 
 class App extends Component {
   render() {
+    let x = 0;
     return (
       <div className={styles.app}>
         <div className={styles.background}>
-          <input
-            className={styles.mainInput}
-            placeholder="What are your preferences?"
-          />
+          <div className={styles.topContainer}>
+            <input
+              className={styles.mainInput}
+              placeholder="What are your preferences?"
+            />
+            <div className={styles.preferencesContainer}>
+              {new Array(20).fill(0).map(_i => (
+                <div key={x++} className={styles.preference}>
+                  Some Preference{" "}
+                  <button className={styles.unstyledButton}>
+                    <i className="fas fa-times" />
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     );
