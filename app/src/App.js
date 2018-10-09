4fcc1ada581dfc95cc648cda544b88a25cd01cf8
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-10-09 16:46:44 +0400

    Move filters to sidebar on right

--- app/src/App.js
+++ app/src/App.js
@@ -5,8 +5,8 @@ class App extends Component {
   render() {
     let x = 0;
     return (
-      <div className={styles.app}>
-        <div className={styles.background}>
+      <div className={styles.appContainer}>
+        <div className={styles.map}>
           <div className={styles.topContainer}>
             <div className={styles.inputContainer}>
               <input
@@ -16,19 +16,22 @@ class App extends Component {
               <div className={styles.suggestion}>Select Location</div>
               <div className={styles.suggestion}>Select Price Range</div>
             </div>
-            <div className={styles.preferencesContainer}>
-              {new Array(8).fill(0).map(_i => (
-                <div key={x++} className={styles.preference}>
-                  <div className={`${styles.iconContainer} ${styles.gymIcon}`}>
-                    <i className="fas fa-dumbbell" />
-                  </div>{" "}
-                  Gym Nearby{" "}
-                  <button className={styles.unstyledButton}>
-                    <i className="fas fa-times" />
-                  </button>
+          </div>
+        </div>
+        <div className={styles.sidebarContainer}>
+          <h2>Filters</h2>
+          <div>
+            {new Array(8).fill(0).map(_i => (
+              <div key={x++} className={styles.preference}>
+                <div className={`${styles.iconContainer} ${styles.gymIcon}`}>
+                  <i className="fas fa-dumbbell" />
                 </div>
-              ))}
-            </div>
+                Gym Nearby
+                <button className={styles.unstyledButton}>
+                  <i className="fas fa-times" />
+                </button>
+              </div>
+            ))}
           </div>
         </div>
       </div>
