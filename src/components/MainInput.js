50a1e56083b95d3acac1dd9adc52707db3acad63
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-11-01 12:47:36 +0400

    Make it a normal input as default

--- src/components/MainInput.js
+++ src/components/MainInput.js
@@ -3,18 +3,29 @@ import styles from "./MainInput.module.scss";
 
 export default class MainInput extends Component {
   render() {
-    return (
-      <div className={styles.inputContainer}>
-        <div
-          className={styles.mainInput}
-          placeholder="What are you looking for?"
-        >
-          Between <input type="number" /> and <input type="number" />{" "}
-          <input type="text" defaultValue="USD" />
+    if (true) {
+      return (
+        <div className={styles.inputContainer}>
+          <input
+            className={styles.mainInput}
+            placeholder="What are you looking for?"
+          />
         </div>
-        <div className={styles.suggestion}>Select Location</div>
-        <div className={styles.suggestion}>Select Price Range</div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className={styles.inputContainer}>
+          <div
+            className={styles.mainInput}
+            placeholder="What are you looking for?"
+          >
+            Between <input type="number" /> and <input type="number" />{" "}
+            <input type="text" defaultValue="USD" />
+          </div>
+          <div className={styles.suggestion}>Select Location</div>
+          <div className={styles.suggestion}>Select Price Range</div>
+        </div>
+      );
+    }
   }
 }
