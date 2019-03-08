f56b25194740243f5fa7c7ede8f7b82f48ce3c19
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2019-03-08 10:25:45 +0400

    Create simple logarithmic scale

--- src/components/ChooseDistanceModal.js
+++ src/components/ChooseDistanceModal.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
+import { Range } from "rc-slider";
 
 export default class ChooseDistanceModal extends Component {
   render() {
-    return <div>Hi</div>;
+    const marks = { 0: "0m" };
+    for (let i = 1; i <= 50 / 10; i++) {
+      const label = i > 2 ? `${10 ** (i - 3)}km` : `${10 ** i}m`;
+      marks[i * 10] = label;
+    }
+    console.log(marks);
+    return (
+      <div>
+        <Range
+          min={0}
+          max={50}
+          count={1}
+          pushable
+          value={[0, 50]}
+          marks={marks}
+        />
+      </div>
+    );
   }
 }
