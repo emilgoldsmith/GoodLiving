856396d180cb2aff103f32da63e0855a9269d00b
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2019-03-08 10:54:32 +0400

    Got logarithmic values working

--- src/components/ChooseDistanceModal.js
+++ src/components/ChooseDistanceModal.js
@@ -5,11 +5,20 @@ export default class ChooseDistanceModal extends Component {
   constructor(...props) {
     super(...props);
     this.minPos = 0;
-    this.maxPos = 50;
+    this.maxPos = 5;
     this.state = {
       posRange: [this.minPos, this.maxPos]
     };
   }
+
+  getValueRange = () => {
+    return this.state.posRange.map(this.posToValue);
+  };
+
+  posToValue = position => {
+    if (position === 0) return 0;
+    return 10 ** position;
+  };
 
   handleRangeChange = newRange => {
     this.setState({ posRange: newRange });
@@ -17,9 +26,9 @@ export default class ChooseDistanceModal extends Component {
 
   render() {
     const marks = { 0: "0m" };
-    for (let i = 1; i <= this.maxPos / 10; i++) {
+    for (let i = 1; i <= this.maxPos; i++) {
       const label = i > 2 ? `${10 ** (i - 3)}km` : `${10 ** i}m`;
-      marks[i * 10] = label;
+      marks[i] = label;
     }
     return (
       <div>
@@ -27,12 +36,17 @@ export default class ChooseDistanceModal extends Component {
           min={this.minPos}
           max={this.maxPos}
           count={1}
-          step={0.1}
+          step={0.01}
           marks={marks}
           pushable
           value={this.state.posRange}
           onChange={this.handleRangeChange}
         />
+        {this.getValueRange().map(x => (
+          <div key={x} style={{ padding: "100px" }}>
+            {x}
+          </div>
+        ))}
       </div>
     );
   }
